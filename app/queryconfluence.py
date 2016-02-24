af472b1fcedc5a1af31d1f32b3a34499eadf9b76
haloz <haloz@gmx.net>
2016-02-24 23:31:04 +0100

    add RESTful client for modifying a Confluence page add unit tests

---
+++ app/queryconfluence.py
@@ -0,0 +1,35 @@
+import requests
+from urllib.parse import urljoin
+
+
+class QueryConfluence(object):
+
+    def packContent(self, page_id, content):
+        return {
+            "type": "page",
+            "id": page_id,
+            "body": {
+                "storage": {
+                    "value": content
+                }
+            },
+            "id": page_id,
+        }
+
+    def checkPageExists(self, server_url, page_id):
+        response = requests.get(
+            urljoin(server_url, str(page_id))
+        )
+        if response.status_code == 200:
+            return True
+        else:
+            return False
+
+    def replacePageContent(self, server_url, page_id, content):
+        if self.checkPageExists:
+            requests.post(
+                urljoin(server_url, str(page_id)),
+                self.packContent(page_id, content),
+                auth=('admin', 'admin'),
+                headers=({'Content-Type': 'application/json'})
+            )
---
+++ tests/queryconfluence_unit_test.py
@@ -0,0 +1,61 @@
+import unittest
+from unittest import mock
+from urllib.parse import urljoin
+from app.queryconfluence import QueryConfluence
+
+
+class MockOkResponse:
+    status_code = 200
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+
+class MockNotFoundResponse:
+    status_code = 404
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+
+class QueryConfluenceTestCase(unittest.TestCase):
+
+    qc = None
+
+    def setUp(self):
+        self.qc = QueryConfluence()
+
+    def testPackContent(self):
+        data = self.qc.packContent(123, "<h1>Test</h1>")
+        self.assertEqual(data["body"]["storage"]["value"], "<h1>Test</h1>")
+
+    def testCheckPageExists(self):
+        with mock.patch(
+            "app.queryconfluence.requests.get",
+            new=MockNotFoundResponse
+        ):
+            self.assertFalse(self.qc.checkPageExists(
+                "http://localhost:8080/confluence/rest/api/content/",
+                123
+            ))
+
+        with mock.patch(
+            "app.queryconfluence.requests.get",
+            new=MockOkResponse
+        ):
+            self.assertTrue(self.qc.checkPageExists(
+                "http://localhost:8080/confluence/rest/api/content/",
+                123
+            ))
+
+    @mock.patch("app.queryconfluence.requests.post")
+    def testReplacePageContent(self, mock_post):
+        url = "http://localhost:8080/confluence/rest/api/content/"
+        data = "<h1>Test</h1>"
+        self.qc.replacePageContent(url, 11, data)
+        mock_post.assert_called_once_with(
+            "http://localhost:8080/confluence/rest/api/content/11",
+            self.qc.packContent(11, data),
+            auth=('admin', 'admin'),
+            headers=({'Content-Type': 'application/json'})
+        )
