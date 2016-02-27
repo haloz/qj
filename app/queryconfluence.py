d68baaa0d17f5205400c3da7d945e842f8bcbf48
haloz <haloz@gmx.net>
2016-02-27 11:46:22 +0100

    refactor pylint add docstrings

--- app/queryconfluence.py
+++ app/queryconfluence.py
@@ -1,10 +1,34 @@
-import requests
+"""This module handles access to Atlassian Confluence"""
+
 from urllib.parse import urljoin
+import requests
+from requests_oauthlib import OAuth1Session
 
 
 class QueryConfluence(object):
+    """Access to Confluence to change the content of a page"""
 
-    def packContent(self, page_id, content):
+    _request_token_url = "/plugins/servlet/oauth/request-token"
+    _authorization_url = "/plugins/servlet/oauth/authorize"
+    _access_token_url = "/plugins/servlet/oauth/access-token"
+
+    @classmethod
+    def oauth(cls, base_url, token, secret, key):
+        """Authenticate to confluence using OAuth 1.0"""
+        token_url = urljoin(
+            base_url, cls._authorization_url + "?oauth_token=" + token
+        )
+        client = OAuth1Session(
+            key,
+            client_secret=secret,
+            resource_owner_key="",
+            resource_owner_secret="")
+        client.get(token_url)
+
+    @classmethod
+    def pack_content(cls, page_id, content):
+        """Wrap a Confluence page id and a page content into a data structure
+           for the request to Confluence"""
         return {
             "type": "page",
             "id": page_id,
@@ -12,24 +36,24 @@ class QueryConfluence(object):
                 "storage": {
                     "value": content
                 }
-            },
-            "id": page_id,
+            }
         }
 
-    def checkPageExists(self, server_url, page_id):
+    @classmethod
+    def check_page_exists(cls, server_url, page_id):
+        """Check if a Confluence page exists"""
         response = requests.get(
             urljoin(server_url, str(page_id))
         )
-        if response.status_code == 200:
-            return True
-        else:
-            return False
+        return response.status_code == 200
 
-    def replacePageContent(self, server_url, page_id, content):
-        if self.checkPageExists(server_url, page_id):
+    @classmethod
+    def replace_page_content(cls, server_url, page_id, content):
+        """Replaces the content of a Confluence page"""
+        if cls.check_page_exists(server_url, page_id):
             requests.post(
                 urljoin(server_url, str(page_id)),
-                self.packContent(page_id, content),
+                cls.pack_content(page_id, content),
                 auth=('admin', 'admin'),
                 headers=({'Content-Type': 'application/json'})
             )
--- tests/queryconfluence_unit_test.py
+++ tests/queryconfluence_unit_test.py
@@ -1,10 +1,12 @@
+"""This module unit tests the QueryConfluence class"""
+
 import unittest
 from unittest import mock
-from urllib.parse import urljoin
 from app.queryconfluence import QueryConfluence
 
 
 class MockOkResponse:
+    """Mock response class for successful HTTP request"""
     status_code = 200
 
     def __init__(self, *args, **kwargs):
@@ -12,6 +14,7 @@ class MockOkResponse:
 
 
 class MockNotFoundResponse:
+    """Mock response class for failed HTTP request"""
     status_code = 404
 
     def __init__(self, *args, **kwargs):
@@ -19,22 +22,37 @@ class MockNotFoundResponse:
 
 
 class QueryConfluenceTestCase(unittest.TestCase):
-
-    qc = None
+    """Test case for QueryConfluence class"""
+    _qc_instance = None
 
     def setUp(self):
-        self.qc = QueryConfluence()
+        self._qc_instance = QueryConfluence()
 
-    def testPackContent(self):
-        data = self.qc.packContent(123, "<h1>Test</h1>")
+    @mock.patch("app.queryconfluence.OAuth1Session.get")
+    def test_oauth(self, mock_oauthget):
+        """Setup a mock to test OAuth of QueryConfluence class"""
+        self._qc_instance.oauth(
+            "http://localhost:1234",
+            "token",
+            "secret",
+            "key"
+        )
+        mock_oauthget.assert_called_once_with(
+            "http://localhost:1234/plugins/servlet/oauth/authorize?oauth_token=token"
+        )
+
+    def test_pack_content(self):
+        """Test if id and content are correctly wrapped in a data structure"""
+        data = self._qc_instance.pack_content(123, "<h1>Test</h1>")
         self.assertEqual(data["body"]["storage"]["value"], "<h1>Test</h1>")
 
-    def testCheckPageExists(self):
+    def test_check_page_exists(self):
+        """Mock-test to check whether a Confluence page exists"""
         with mock.patch(
             "app.queryconfluence.requests.get",
             new=MockNotFoundResponse
         ):
-            self.assertFalse(self.qc.checkPageExists(
+            self.assertFalse(self._qc_instance.check_page_exists(
                 "http://localhost:8080/confluence/rest/api/content/",
                 123
             ))
@@ -43,23 +61,24 @@ class QueryConfluenceTestCase(unittest.TestCase):
             "app.queryconfluence.requests.get",
             new=MockOkResponse
         ):
-            self.assertTrue(self.qc.checkPageExists(
+            self.assertTrue(self._qc_instance.check_page_exists(
                 "http://localhost:8080/confluence/rest/api/content/",
                 123
             ))
 
     @mock.patch("app.queryconfluence.requests.post")
-    def testReplacePageContent(self, mock_post):
+    def test_replace_page_content(self, mock_post):
+        """Mock-test replacing a Confluence page content"""
         url = "http://localhost:8080/confluence/rest/api/content/"
         data = "<h1>Test</h1>"
         with mock.patch(
             "app.queryconfluence.requests.get",
             new=MockOkResponse
         ):
-            self.qc.replacePageContent(url, 11, data)
+            self._qc_instance.replace_page_content(url, 11, data)
         mock_post.assert_called_once_with(
             "http://localhost:8080/confluence/rest/api/content/11",
-            self.qc.packContent(11, data),
+            self._qc_instance.pack_content(11, data),
             auth=('admin', 'admin'),
             headers=({'Content-Type': 'application/json'})
         )
